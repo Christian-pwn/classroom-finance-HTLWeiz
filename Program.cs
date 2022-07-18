2c7319916c245c3d3390b1e86368b97476cc8aff
Christian Schorna <christian.schorna@hotmail.com>
2022-07-18 21:23:09 +0200

    Added Swagger support

--- Program.cs
+++ Program.cs
@@ -28,6 +28,9 @@ builder.Services.AddRazorPages()
     .AddMicrosoftIdentityUI()
     .AddRazorRuntimeCompilation();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -37,6 +40,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
